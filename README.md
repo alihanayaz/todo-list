ad42fbc6f84ec06a4c2394bcfee8c6689db3fb73
alihanayaz <alihanayaz0@gmail.com>
2023-10-22 22:02:17 +0300

    configure project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# todo-list
+# Todo-List App
+
+- This is a simple todo list app that allows users to manage their tasks, organize them into projects, and persist their data using the Web Storage API (localStorage).
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'assets'),
+    },
+  },
+};
